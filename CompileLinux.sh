49500ceb1c634c818fa5b3e800be281a4c003f4d
Technetium <Technetium1@users.noreply.github.com>
2020-11-24 19:46:22 -0500

    Update CompileLinux.sh

--- CompileLinux.sh
+++ CompileLinux.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 python3.9 -m pip install -U -r requirements.txt
 pyinstaller -F --clean VSCodeExtensionInstaller.py
 cp -f VSCodeExtensions.txt dist/VSCodeExtensions.txt
-chmod +x dist/VSCodeExtensions
+chmod +x dist/VSCodeExtensionInstaller
