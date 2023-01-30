6501f77a200129599d00bd8fb6f82a9cfa6e014c
Technetium1 <Technetium1@users.noreply.github.com>
2023-01-30 17:49:52 -0500

    call python not python3.9

--- CompileLinux.sh
+++ CompileLinux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-python3.9 -m pip install -U -r requirements.txt
+python -m pip install -U -r requirements.txt
 pyinstaller -F --clean VSCodeExtensionInstaller.py
 cp -f VSCodeExtensions.txt dist/VSCodeExtensions.txt
 chmod +x dist/VSCodeExtensionInstaller
